a79f90146d293ca7d92314860a8db7af98024e00
Jack Whitworth <45968087+JMWhitworth@users.noreply.github.com>
2023-07-15 15:48:05 +0100

    Initial release upload

---
+++ solarsystem/app.js
@@ -0,0 +1,94 @@
+import {Application} from 'pixi.js';
+import debug from './solarsystem/helpers';
+import CameraGroup from './solarsystem/CameraGroup';
+import Star from './solarsystem/Star';
+
+
+export default class SolarSystem
+{
+    /**
+     * @param {String} name 
+     */
+    constructor(name)
+    {
+        this._name = name;
+    }
+
+    
+    /**
+     * Returns the 'name' attribute assigned to the instance of the class called
+     * @returns {String} Name
+     */
+    name()
+    {
+        return this._name;
+    }
+}
+
+
+
+
+
+
+
+
+// Create main PIXI.js app
+const app = new Application({
+    background: '#111',
+    resizeTo: window,
+    antialias: true,
+});
+document.body.appendChild(app.view);
+
+// Create camera group to contain moveable sprites
+const camera = new CameraGroup();
+app.stage.addChild(camera);
+
+
+// Testing mouse events on full window
+app.renderer.view.addEventListener('mousedown', function(event) {
+    debug("Start", elapsed);
+    debug("Mousebutton", event.button);
+});
+app.renderer.view.addEventListener('mouseup', function(event) {
+    debug("End", elapsed);
+    debug("Mousebutton", event.button);
+});
+app.renderer.view.addEventListener('touchstart', function(event) {
+    debug("Start", elapsed);
+    debug("Mousebutton", event.button);
+});
+app.renderer.view.addEventListener('touchend', function(event) {
+    debug("End", elapsed);
+    debug("Mousebutton", event.button);
+});
+
+
+// Testing moving the new camera group
+camera.onglobalpointermove = (event) => {
+    debug("pointer", event);
+    if (camera.moveit) {
+        const x = event.data.getLocalPosition(app.stage).x;
+        const y = event.data.getLocalPosition(app.stage).y;
+        camera.position.set(x, y);
+    }
+}
+
+
+// Instantiate stars
+const sun = new Star("#e99000", app.screen.width/10, app.screen.height/2, 40);
+camera.addChild(sun);
+
+const notTheSun = new Star("#e6e6e6", 60, 200, 20);
+camera.addChild(notTheSun);
+
+
+// Main rendering loop
+let elapsed = 0;
+app.ticker.add((delta) =>
+{
+    elapsed++;
+    sun.position.x += 0.1;
+    debug("Frame", elapsed);
+    debug("Camera Pos", camera.position.x +", "+ camera.position.y);
+});
---
+++ solarsystem/helpers.js
@@ -0,0 +1,39 @@
+/**
+ * Creates a div within the debug block on the window with the specified ID.
+ * Repeate calls with the same ID will overwrite the content in the existing div.
+ * @param {string} id The ID to assign the HTML div
+ * @param {*} content The content to set within the HTML div
+ * @returns {boolean} If the function ran as expected
+ */
+export default function debug(id, content = "")
+{
+    if (!document.getElementById('debug') || !validString(id)) {
+        return false;
+    }
+
+    let existingDiv = document.getElementById(id);
+    
+    if (!existingDiv) {
+        const debugDiv = document.getElementById('debug');
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        debugDiv.appendChild(newDiv);
+        existingDiv = newDiv;
+    }
+    existingDiv.innerHTML = id + ": " + content;
+    return true;
+}
+
+/**
+ * Checks that the given variable is a string and isn't blank.
+ * @param {*} stringVariable The variabel to test
+ * @returns {boolean} If the given variable is a valid string
+ */
+export default function validString(stringVariable)
+{
+    if (typeof stringVariable === 'string' && stringVariable.trim().length > 0) {
+      return true;
+    }
+    return false;
+}
+  
